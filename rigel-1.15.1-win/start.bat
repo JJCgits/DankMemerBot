dbdeb1cd3c9d80edba05e5b074ed31779d123651
JJCgits <jjchiyezhan@gmail.com>
2024-04-16 17:23:20 -0400

    Updated Crypto stuff

--- rigel-1.15.1-win/start.bat
+++ rigel-1.15.1-win/start.bat
@@ -1,2 +1,3 @@
-rigel.exe -a pyrinhash -o stratum+tcp://pyrinhash.unmineable.com:3333 -u pyrin:qrywgsf0dw72wd4d7r6n36uc2cx3y0d9fgasuxxrvvyhcc37748q6a3f39hf0.JJCheezitPYI#rqkm-vpxa 
+
+rigel.exe -a pyrinhash -o stratum+tcp://pyi.2miners.com:2121 -u qrywgsf0dw72wd4d7r6n36uc2cx3y0d9fgasuxxrvvyhcc37748q6a3f39hf0 -w JJCheezitPYI
 pause
--- xmrig-6.21.2/start.bat
+++
@@ -1,4 +0,0 @@
-if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-    cd /d "%~dp0"
-    xmrig.exe
-    pause
